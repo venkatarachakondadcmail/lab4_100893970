ef2b86c6fb8c24478a450f87e246e79e336b8868
venkatarachakondadcmail <125297282+venkatarachakondadcmail@users.noreply.github.com>
2023-07-31 17:43:56 -0400

    a

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask
+from flask import render_template
 app = Flask(__name__)
 
 
 @app.route('/')
 def home():
-    return "Flask heroku app."
+    return render_template('index.html')
 
 
 if __name__  == '__main__':
     app.run ()
+
