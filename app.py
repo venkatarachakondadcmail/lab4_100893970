49cf53a230e5f42e0cddb32e32f0ca3367ee51d1
venkatarachakondadcmail <125297282+venkatarachakondadcmail@users.noreply.github.com>
2023-07-31 17:51:03 -0400

    a

--- app.py
+++ app.py
@@ -1,30 +1,30 @@
 from flask import Flask, request, jsonify
-import joblib
+# import joblib
 from flask import render_template
 
 
 app = Flask(__name__)
-model = joblib.load('fish_weight_model.pkl')
+# model = joblib.load('fish_weight_model.pkl')
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/predict', methods=['POST'])
-def predict_weight():
-    # Load the traned model
-    data = request.get_json()
-    length_ver = data['length_ver']
-    length_dia = data['length_dia']
-    length_cro = data['length_cro']
-    height = data['height']
-    width = data['width']
+# @app.route('/predict', methods=['POST'])
+# def predict_weight():
+#     # Load the traned model
+#     data = request.get_json()
+#     length_ver = data['length_ver']
+#     length_dia = data['length_dia']
+#     length_cro = data['length_cro']
+#     height = data['height']
+#     width = data['width']
 
-    prediction_data = [[length_ver, length_dia, length_cro, height, width]]
-    prediction = model.predict(prediction_data)[0]
+#     prediction_data = [[length_ver, length_dia, length_cro, height, width]]
+#     prediction = model.predict(prediction_data)[0]
 
-    return jsonify({'predicted_weight': prediction})
+#     return jsonify({'predicted_weight': prediction})
 
 if __name__ == '__main__':
     app.run()
